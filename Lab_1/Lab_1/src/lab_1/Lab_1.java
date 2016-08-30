d30747613d35509ff8e9757360d077a4ddc358a2
gemarkees <gemarkees@vassar.edu>
2016-08-30 16:06:44 -0400

    First snapshot

---
+++ Lab_1/Lab_1/src/lab_1/Lab_1.java
@@ -0,0 +1,19 @@
+/*
+ * Simple program that prints out the words "Hello World"
+ */
+package lab_1;
+
+/**
+ *
+ * @author gemarkees
+ */
+public class Lab_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+    
+}
